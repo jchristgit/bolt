248ddcc2c307a4a15a64edb3a0a8f474ebe7c568
Volcyy <Johannes.christ@gmail.com>
2017-05-12 08:41:11 +0200

    add basic administration cog

--- src/cogs/administration.py
+++ src/cogs/administration.py
@@ -1,5 +1,6 @@
 import logging
 
+from discord import Embed
 from discord.ext import commands
 
 log = logging.getLogger('bot')
@@ -9,12 +10,17 @@ class Administration:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.is_owner()
     @commands.command(hidden=True)
     async def shutdown(self, ctx):
         """Shutdown the Bot. Owner only."""
-        await ctx.send('Shutting down')
+        await ctx.send(embed=Embed(description='Shutting down...'))
         await self.bot.close()
 
 
 def setup(bot):
     bot.add_cog(Administration(bot))
+
+
+def teardown():
+    print('Unloaded Cog Administration')
