2574baebe24dfd2861572ebbad4f8a26fff4cc36
Volcyy <Johannes.christ@gmail.com>
2017-05-12 08:32:59 +0200

    add bot logger

--- run.py
+++ run.py
@@ -7,20 +7,26 @@ from builtins import ModuleNotFoundError
 from discord.ext import commands
 from os import environ, makedirs
 
-makedirs('logs', exist_ok=True)
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 # Set up Logging
+makedirs('logs', exist_ok=True)
+standard_formatter = logging.Formatter('[%(levelname)s] %(asctime)s (%(name)s): %(message)s')
 discord_logger = logging.getLogger('discord')
 discord_logger.setLevel(logging.INFO)
-discord_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-discord_handler.setFormatter(logging.Formatter('[%(levelname)s] %(asctime)s:%(name)s: %(message)s'))
+discord_handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+discord_handler.setFormatter(standard_formatter)
 discord_logger.addHandler(discord_handler)
+bot_logger = logging.getLogger('bot')
+bot_logger.setLevel(logging.INFO)
+bot_handler = logging.FileHandler(filename='logs/bot.log', encoding='utf-8', mode='w')
+bot_handler.setFormatter(standard_formatter)
+bot_logger.addHandler(bot_handler)
 
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(['!']), description='beep bop', pm_help=None)
+        super().__init__(command_prefix=commands.when_mentioned_or('!', '?'), description='beep bop', pm_help=None)
         self.start_time = datetime.datetime.now()
 
     async def on_ready(self):
@@ -29,16 +35,12 @@ class Bot(commands.Bot):
         print(f'ID: {self.user.id}')
         print(f'Connected to {len(self.guilds)} Guilds.')
         print(f'Connected to {len(self.users)} Users.')
-        print(f'Invite Link:\nhttps://discordapp.com/oauth2/authorize?&client_id={self.user.id}&scope=discordbot')
+        print(f'Invite Link:\nhttps://discordapp.com/oauth2/authorize?&client_id={self.user.id}&scope=bot')
         print('=============')
 
     async def on_message(self, msg):
         if msg.author.bot:
             return
-
-        # TODO: make Discord Bot only shutdown on shutdown command, since ^C not good
-        if msg.content == '!shutdown':
-            await self.close()
 
         await self.process_commands(msg)
 
@@ -51,7 +53,7 @@ COGS_BASE_PATH = 'src.cogs.'
 
 # Cogs to load on login
 COGS_ON_LOGIN = [
-    'roles'
+    'administration'
 ]
 
 
@@ -63,11 +65,16 @@ if __name__ == '__main__':
         except ModuleNotFoundError as err:
             print(f'Could not load Cog \'{cog}\': {err}.')
         else:
-            print(f'Loaded Cog {cog}.')
-
+            print(f'Loaded Cog {cog.title()}.')
 
     print('Logging in...')
     client.run(environ['DISCORD_TOKEN'])
     print('Logged off.')
+
+    # Close Logging
+    discord_handler.close()
+    bot_handler.close()
+    discord_logger.removeHandler(discord_handler)
+    bot_logger.removeHandler(discord_handler)
 
 
---
+++ src/cogs/administration.py
@@ -0,0 +1,20 @@
+import logging
+
+from discord.ext import commands
+
+log = logging.getLogger('bot')
+
+
+class Administration:
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(hidden=True)
+    async def shutdown(self, ctx):
+        """Shutdown the Bot. Owner only."""
+        await ctx.send('Shutting down')
+        await self.bot.close()
+
+
+def setup(bot):
+    bot.add_cog(Administration(bot))
