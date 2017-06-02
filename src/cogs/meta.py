9cbe7e0aaffa2c6e6ecc8fdde5cfb335a074327a
Volcyy <Johannes.christ@gmail.com>
2017-06-02 02:08:45 +0200

    use integer division instead of casting to int

--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -42,18 +42,18 @@ class Meta:
         """Displays information and statistics about the Bot."""
 
         memory_usage = self.process.memory_full_info().uss / 1024**2 * 1.048576
-        average_guild_users = sum(g.member_count for g in self.bot.guilds) / len(self.bot.guilds)
+        average_guild_users = sum(g.member_count for g in self.bot.guilds) // len(self.bot.guilds)
 
         response = discord.Embed()
         response.colour = discord.Colour.blue()
         response.set_thumbnail(url=self.bot.user.avatar_url)
         response.set_author(name='Made by Volcyy#2359', url='https://github.com/Volcyy/Bolt',
                             icon_url=(await self.bot.application_info()).owner.avatar_url)
-        response.add_field(name='Users', value=f'**Total**: {sum(1 for m in self.bot.get_all_members())}\n'
+        response.add_field(name='Users', value=f'**Total**: {sum(1 for _ in self.bot.get_all_members())}\n'
                                                f'**Unique**: {len(set(self.bot.get_all_members()))}')
 
-        response.add_field(name='Guilds', value=f'**Total**: {sum(1 for g in self.bot.guilds)}\n'
-                                                f'**Avg. Members**: {int(average_guild_users)}\n'
+        response.add_field(name='Guilds', value=f'**Total**: {sum(1 for _ in self.bot.guilds)}\n'
+                                                f'**Avg. Members**: {average_guild_users}\n'
                                                 f'**Shards**: {self.bot.shard_count}')
         response.add_field(name='System Usage', value=f'**CPU**: {self.process.cpu_percent() / psutil.cpu_count()}%\n'
                                                       f'**Memory**: {str(memory_usage)[:5]} MB')
