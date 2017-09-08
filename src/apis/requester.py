57bcbbad4c97abcd7389f060a33f07c4eaca12f3
Volcyy <Johannes.christ@gmail.com>
2017-09-08 14:58:35 +0200

    Cleanup requester.

--- src/apis/requester.py
+++ src/apis/requester.py
@@ -1,26 +1,13 @@
 import aiohttp
 
-from ..util import create_logger
-
-log = create_logger('api')
-
 
 class NotFoundError(Exception):
     pass
 
 
-class Requester:
-    def __init__(self):
-        self._cs = None
-
-    def __del__(self):
-        if self._cs is not None:
-            self._cs.close()
-
-    async def get(self, url: str, headers: list) -> dict:
-        if self._cs is None:
-            self._cs = aiohttp.ClientSession()
-        async with self._cs.get(url, headers=headers) as r:
+async def get(url: str, headers: list) -> dict:
+    async with aiohttp.ClientSession as cs:
+        async with cs.get(url, headers=headers) as r:
             r.raise_for_status()
             if r.status == 200:
                 return await r.json()
