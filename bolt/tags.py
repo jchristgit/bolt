e4626797f19a7ad4efd52cef0a8b74d1e2acdd7d
Volcyy <johannes.christ@gmail.com>
2017-09-28 20:53:05 +0200

    Add tag delete command.

--- bolt/tags.py
+++ bolt/tags.py
@@ -29,6 +29,7 @@ class Tags:
         To view a tag, simply use this command along with a tag name.
         """
 
+        tag_name = tag_name.title()
         tag = self._tag_table.find_one(guild_id=ctx.guild.id, name=tag_name)
         if tag is None:
             await ctx.send(embed=discord.Embed(
@@ -66,23 +67,64 @@ class Tags:
             tag create 'my tag name' tag content
         """
 
+        tag_name = tag_name.title()
         if self._tag_table.find_one(guild_id=ctx.guild.id,
-                                    tag_content=tag_content) is not None:
+                                    tag_name=tag_name) is not None:
             await ctx.send(embed=discord.Embed(
-                title="A Tag with the given contents already exists.",
+                title="A Tag with the given name already exists.",
                 colour=discord.Colour.red()
             ))
         else:
             self._tag_table.insert(dict(
                 guild_id=ctx.guild.id,
                 author_id=ctx.message.author.id,
-                name=tag_name,
+                name=tag_name.title(),
                 content=tag_content,
                 created_on=datetime.datetime.utcnow()
             ))
             await ctx.send(embed=discord.Embed(
                 title=f"Created the tag {tag_name!r}!",
                 colour=discord.Colour.green()
+            ))
+
+    @tag.command()
+    @commands.guild_only()
+    async def delete(self, ctx, *, tag_name: str):
+        """Delete the specified tag.
+
+        The tag needs to be created on the guild this is
+        invoked, and the author needs to either have the
+        'manage messages' permission or should be the
+        original creator of the tag.
+        """
+
+        tag_name = tag_name.title()
+        tag = self._tag_table.find_one(guild_id=ctx.guild.id,
+                                       tag_name=tag_name)
+        if tag is not None:
+            if ctx.author.id != tag.author_id \
+               and not ctx.author.permissions_in(ctx.channel).manage_messages:
+                await ctx.send(embed=discord.Embed(
+                    title="Failed to delete tag",
+                    description=("You need to either have the 'Manage "
+                                 "Messages' permission or need to be "
+                                 "the creator of the specified tag."),
+                    colour=discord.Colour.red()
+                ))
+            else:
+                self._tag_table.delete(
+                    guild_id=ctx.guild.id,
+                    tag_name=tag_name
+                )
+                await ctx.send(embed=discord.Embed(
+                    title=f"Deleted the tag {tag_name!r}.",
+                    colour=discord.Colour.green()
+                ))
+        else:
+            await ctx.send(embed=discord.Embed(
+                title="Failed to delete tag",
+                description=f"No tag named {tag_name!r} found on this Guild.",
+                colour=discord.Colour.red()
             ))
 
 
