54e2b238f8a86e93cb3e38222fef1ffb37362ac0
Volcyy <johannes.christ@gmail.com>
2018-01-27 20:37:47 +0100

    Add model for optional cogs.

--- bolt/models.py
+++ bolt/models.py
@@ -30,3 +30,10 @@ prefix = Table('prefix', metadata,
     Column('guild_id', BigInteger(), primary_key=True),
     Column('prefix', String(50), nullable=False)
 )
+
+
+opt_cog = Table('optional_cog', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(20), nullable=False),
+    Column('guild_id', BigInteger(), nullable=False)
+)
