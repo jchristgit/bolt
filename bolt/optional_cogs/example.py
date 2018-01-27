17a4fd41f0e5da3dd9cc1ddf5913054bac3d6903
Volcyy <johannes.christ@gmail.com>
2018-01-27 21:48:05 +0100

    Add optional cog package.

---
+++ bolt/optional_cogs/example.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+from .base import OptionalCog
+
+
+class Example(OptionalCog):
+    """
+    An example file to demonstrate
+    the function of optional cog usage.
+    """
+
+    RESTRICTED = False
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send("Hello from an optional cog!")
+
+
+def setup(bot):
+    bot.add_cog(Example(bot))
