952ec1ec4dfe010489a8dddba567cf4e503a15a4
Volcyy <johannes.christ@gmail.com>
2018-05-05 20:24:50 +0200

    Remove automatic table creation.
    Father forgive, for I have sinned
    
    Signed-off-by: Volcyy <johannes.christ@gmail.com>

--- bolt/database.py
+++ bolt/database.py
@@ -1,30 +1,8 @@
-import inspect
-import itertools
-
-from sqlalchemy import Table, create_engine, MetaData
-from sqlalchemy.schema import CreateTable
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy_aio import ASYNCIO_STRATEGY
-
-from . import cogs, optional_cogs
 
 
 engine = create_engine(
     'sqlite:///data/bolt.db', strategy=ASYNCIO_STRATEGY
 )
 metadata = MetaData()
-
-
-def get_cog_models():
-    for _, cog_pkg in itertools.chain(
-            inspect.getmembers(cogs, inspect.ismodule),
-            inspect.getmembers(optional_cogs, inspect.ismodule)):
-        for name, cog_module in inspect.getmembers(cog_pkg, inspect.ismodule):
-            if name == 'models':
-                for _, table in inspect.getmembers(cog_module, lambda v: isinstance(v, Table)):
-                    yield table
-
-
-async def setup():
-    for table in get_cog_models():
-        if not await engine.has_table(table.name):
-            await engine.execute(CreateTable(table))
