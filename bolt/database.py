92d2bfdea47f74fea137553e84bc98c587f2cc32
Volcyy <johannes.christ@gmail.com>
2018-05-06 18:44:34 +0200

    Parse database URL properly.
    Signed-off-by: Volcyy <johannes.christ@gmail.com>

--- bolt/database.py
+++ bolt/database.py
@@ -1,11 +1,19 @@
 import asyncio
 import os
+
 import peewee
 from peewee_async import Manager, PostgresqlDatabase
+from playhouse import db_url
 
 
 engine_url = os.environ['BOLT_DATABASE_URL']
-database = PostgresqlDatabase(engine_url)
+database_config = db_url.parse(engine_url)
+database = PostgresqlDatabase(
+    database_config['database'],
+    user=database_config['user'],
+    password=database_config.get('password') or '',
+    host=database_config['host']
+)
 objects = Manager(database, loop=asyncio.get_event_loop())
 
 
