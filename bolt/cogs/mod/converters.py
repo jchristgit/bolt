a6500e0c54a68e0bbd21723e997ebfedfa1f7fff
Johannes Christ <johannes.christ@gmail.com>
2018-05-10 12:50:15 +0200

    Simplify date parsing when no "direction" is given.

--- bolt/cogs/mod/converters.py
+++ bolt/cogs/mod/converters.py
@@ -4,17 +4,19 @@ import dateparser
 from discord.ext.commands import BadArgument, Converter
 
 
+DATEPARSER_SETTINGS = {
+    'PREFER_DATES_FROM': 'future',
+    'TO_TIMEZONE': 'UTC'
+}
+
+
 class ExpirationDate(Converter):
     async def convert(self, ctx, expiration_string: str):
-        expiry = dateparser.parse(expiration_string, settings={'TO_TIMEZONE': 'UTC'})
+        expiry = dateparser.parse(expiration_string, settings=DATEPARSER_SETTINGS)
         if expiry is None:
-            raise BadArgument(f"Failed to parse expiration date `{expiration_string}`.")
+            raise BadArgument(f"Failed to parse expiration date from `{expiration_string}`")
 
-        # Check if the given expiration date lies within the past.
-        # This usually happens when not explicitly specifying whether
-        # the date is within the future or in the past, for example "2 hours".
-        now = datetime.utcnow()
-        if expiry < now:
-            expiry = now + (now - expiry)
+        if expiry < datetime.utcnow():
+            raise BadArgument("Expiration date cannot be within the past")
 
         return expiry
