94672a1e0d65a89357dbeb6b30ba652e60d901b9
Johannes Christ <johannes.christ@gmail.com>
2018-05-31 17:00:18 +0200

    Log member join / remove and message delete events.

--- bolt/cogs/stafflog/cog.py
+++ bolt/cogs/stafflog/cog.py
@@ -1,9 +1,17 @@
+import logging
+from datetime import datetime
+from typing import Optional, Tuple
+
 import discord
+import humanize
 from discord.ext import commands
 from peewee import DoesNotExist
 
 from bolt.database import objects
 from .models import StaffLogChannel
+
+
+log = logging.getLogger(__name__)
 
 
 class StaffLog:
@@ -15,6 +23,125 @@ class StaffLog:
 
     def __init__(self, bot):
         self.bot = bot
+
+    async def get_log_channel(self, guild: discord.Guild) -> Optional[
+        Tuple[
+            StaffLogChannel,
+            discord.TextChannel
+            ]]:
+        """
+        Get the staff log channel for the given Guild ID.
+
+        Args:
+            guild_id (discord.Guild):
+                The guild whose log channel should be returned.
+
+        Returns:
+            Optional[Tuple[StaffLogChannel, discord.TextChannel]]:
+                The channel row and discord Channel if found,
+                otherwise, if nothing was found, `None`.
+        """
+
+        try:
+            channel_row = await objects.get(
+                StaffLogChannel,
+                guild_id=guild.id
+            )
+        except DoesNotExist:
+            return None
+        else:
+            channel = guild.get_channel(channel_row.channel_id)
+            if channel is None:
+                log.debug(
+                    "Previously set stafflog channel for guild {guild} ({guild.id}) "
+                    "could not be found anymore, deleting from the database."
+                )
+                await objects.delete(channel_row)
+            return channel_row, channel
+
+    async def log_for(self, guild: discord.Guild, embed: discord.Embed):
+        """
+        Log the given embed in the given guild's staff log channel, if set.
+
+        Args:
+            guild (discord.Guild):
+                The guild to log the event on.
+            embed (discord.Embed):
+                The embed to send in the staff log channel.
+        """
+
+        channel_row, channel = await self.get_log_channel(guild)
+        if channel_row.enabled and channel is not None:
+            await channel.send(embed=embed)
+
+    async def on_message_delete(self, message: discord.Message):
+        if message.guild is None or message.author == self.bot.user:
+            return
+
+        info_embed = discord.Embed(
+            title=f"🗑 Message deleted (`{message.id}`)",
+            colour=discord.Colour.red(),
+            timestamp=datetime.utcnow()
+        ).set_author(
+            name=f"{message.author} ({message.author.id})",
+            icon_url=message.author.avatar_url
+        ).add_field(
+            name="Channel",
+            value=message.channel.mention
+        ).add_field(
+            name="Creation date",
+            value=message.created_at.strftime('%d.%m.%y %H:%M')
+        ).add_field(
+            name="System content",
+            value=message.system_content
+        )
+
+        if message.attachments:
+            info_embed.add_field(
+                name=f"{len(message.attachments)} Attachments",
+                value=', '.join(
+                    f"[{attachment.filename}]({attachment.url})"
+                    for attachment in message.attachments
+                )
+            )
+
+        await self.log_for(message.guild, info_embed)
+
+    async def on_member_join(self, member: discord.Member):
+        info_embed = discord.Embed(
+            title=f"📥 Member joined",
+            colour=discord.Colour.green(),
+            timestamp=datetime.utcnow()
+        ).set_thumbnail(
+            url=member.avatar_url
+        ).add_field(
+            name="User",
+            value=f"`{member}` (`{member.id}`)"
+        ).add_field(
+            name="Account creation",
+            value=f"{member.created_at.strftime('%d.%m.%y %H:%M')} UTC "
+                  f"({humanize.naturaldelta(datetime.utcnow() - member.created_at)} ago)"
+        )
+
+        await self.log_for(member.guild, info_embed)
+
+    async def on_member_remove(self, member: discord.Member):
+        info_embed = discord.Embed(
+            title=f"📤 Member left",
+            colour=discord.Colour.red(),
+            timestamp=datetime.utcnow()
+        ).set_thumbnail(
+            url=member.avatar_url
+        ).add_field(
+            name="User",
+            value=f"`{member}` (`{member.id}`)"
+        ).add_field(
+            name="Joined at",
+            value=f"{member.joined_at.strftime('%d.%m.%y %H:%M')} UTC "
+                  f"({humanize.naturaldelta(datetime.utcnow() - member.joined_at)} ago)"
+        )
+
+        await self.log_for(member.guild, info_embed)
 
     @commands.group(name='log', aliases=['stafflog'])
     @commands.has_permissions(manage_messages=True)
