1757ea8d80a4782a26e760608204b986dcef0610
Johannes Christ <johannes.christ@gmail.com>
2018-06-05 18:23:05 +0200

    Fix a `KeyError` due to unconfigured botlog.

--- bolt/cogs/botlog/cog.py
+++ bolt/cogs/botlog/cog.py
@@ -23,30 +23,30 @@ class BotLog:
     async def on_ready(self):
         if 'BOTLOG_CHANNEL_ID' not in environ:
             log.warning("No bot log channel is set, bot logging will NOT be enabled.")
-
-        log_channel_id = environ['BOTLOG_CHANNEL_ID']
-        try:
-            self.channel = self.bot.get_channel(int(log_channel_id))
-        except ValueError:
-            log.error(f"{log_channel_id} is not a valid channel ID, must be an integer")
         else:
-            if self.channel is None:
-                log.error(f"Failed to find bot log channel under ID {log_channel_id}")
+            log_channel_id = environ['BOTLOG_CHANNEL_ID']
+            try:
+                self.channel = self.bot.get_channel(int(log_channel_id))
+            except ValueError:
+                log.error(f"{log_channel_id} is not a valid channel ID, must be an integer")
             else:
-                info_embed = Embed(
-                    title="Logged in and ready",
-                    colour=Colour.green()
-                ).add_field(
-                    name="Total members",
-                    value=f"`{len(self.bot.users)}`"
-                ).add_field(
-                    name="Total guilds",
-                    value=f"`{len(self.bot.guilds)}`"
-                ).add_field(
-                    name="Total commands",
-                    value=f"`{len(self.bot.commands)}`"
-                )
-                await self.channel.send(embed=info_embed)
+                if self.channel is None:
+                    log.error(f"Failed to find bot log channel under ID {log_channel_id}")
+                else:
+                    info_embed = Embed(
+                        title="Logged in and ready",
+                        colour=Colour.green()
+                    ).add_field(
+                        name="Total members",
+                        value=f"`{len(self.bot.users)}`"
+                    ).add_field(
+                        name="Total guilds",
+                        value=f"`{len(self.bot.guilds)}`"
+                    ).add_field(
+                        name="Total commands",
+                        value=f"`{len(self.bot.commands)}`"
+                    )
+                    await self.channel.send(embed=info_embed)
 
     async def on_guild_join(self, guild: Guild):
         if self.channel is not None:
