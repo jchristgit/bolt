ed3b23d4cd67de5646677de581024697028cd0c8
Johannes Christ <johannes.christ@gmail.com>
2018-06-12 00:23:09 +0200

    Use personal fork for `Alchemy` for now.

--- mix.exs
+++ mix.exs
@@ -22,7 +22,9 @@ defmodule Bolt.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:alchemy, "~> 0.6.0", hex: :discord_alchemy}
+      # {:alchemy, "~> 0.6.0", hex: :discord_alchemy}
+      {:alchemy,
+       git: "https://github.com/jchristgit/alchemy", branch: "add-guild-icon-url-function"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
