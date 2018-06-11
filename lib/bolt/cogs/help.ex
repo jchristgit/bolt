3423bad8bb85589965fbc528d6524e2c234c514d
Johannes Christ <johannes.christ@gmail.com>
2018-06-12 00:24:12 +0200

    Show detailed command help including arguments.

--- lib/bolt/cogs/help.ex
+++ lib/bolt/cogs/help.ex
@@ -4,19 +4,87 @@ defmodule Bolt.Cogs.Help do
   require Alchemy.Embed
   alias Bolt.Constants
 
-  @spec get_command_information(String.t()) :: {Module.t(), arity(), String.t()}
-  defp get_command_information(command_name) do
-    case Cogs.all_commands()[command_name] do
-      {module, arity, function_name, parser} -> {module, arity, function_name}
+  @spec command_information(String.t()) :: {Module.t(), arity(), String.t()}
+  defp command_information(cmd_name) do
+    IO.inspect(Cogs.all_commands())
+
+    case Cogs.all_commands()[cmd_name] do
+      {module, arity, function_name, _parser} -> {module, arity, function_name}
       res -> res
     end
   end
 
-  @spec format_command_help(Module.t(), integer(), String.t()) :: Embed.t()
-  defp format_command_help(_module, _arity, function_name) do
+  @spec command_doc_base(Module.t(), arity(), String.t()) ::
+          {{String.t(), arity()}, integer(), atom(), list(), String.t()}
+  def command_doc_base(module, _cmd_arity, cmd_name) do
+    module
+    |> Code.get_docs(:docs)
+    |> Enum.find(fn {{name, _arity}, _, _, _args, _doc} -> cmd_name == name end)
+  end
+
+  @spec format_args(list()) :: String.t()
+  defp format_args(args) do
+    args
+    |> Stream.drop(1)
+    |> Stream.map(fn val ->
+      case val do
+        {_, _, [{arg_name, _, _}, default]} -> "<#{arg_name}[=#{default}]>"
+        {arg_name, _, _} -> "<#{arg_name}>"
+        val -> "#{inspect(val)}"
+      end
+    end)
+    |> Enum.join(" ")
+  end
+
+  @spec command_overview() :: Enumerable.t()
+  defp command_overview do
+    Cogs.all_commands()
+    |> Map.values()
+    |> Stream.map(fn cmd_info ->
+      {module, arity, function_name} =
+        case cmd_info do
+          {module, arity, function_name} -> {module, arity, function_name}
+          {module, arity, function_name, _parser} -> {module, arity, function_name}
+        end
+
+      command_doc_base(module, arity, function_name)
+      |> (fn {{name, _arity}, _, _, args, doc} ->
+            formatted_args = format_args(args)
+            arg_string = if formatted_args != "", do: " `#{formatted_args}`", else: ""
+
+            if doc == nil do
+              "**`#{name}`**#{arg_string}"
+            else
+              "**`#{name}`**#{arg_string}\n#{hd(String.split(doc, "\n"))}"
+            end
+          end).()
+    end)
+    |> Enum.join("\n\n")
+    |> IO.inspect()
+  end
+
+  @spec format_command_overview() :: Embed.t()
+  def format_command_overview do
     %Embed{
-      title: "`#{function_name}`",
-      description: "Maybe I could provide documentation here.",
+      title: "command overview",
+      description: command_overview(),
+      color: Constants.color_blue()
+    }
+    |> Embed.footer(text: "use `help <command>` for more details")
+  end
+
+  @spec format_command_detail(Module.t(), arity(), String.t()) :: String.t() | nil
+  defp format_command_detail(module, cmd_arity, cmd_name) do
+    {{name, arity}, _, _, args, doc} = command_doc_base(module, cmd_arity, cmd_name)
+
+    %Embed{
+      title: "❔ `#{name}/#{arity - 1}`",
+      description: """
+      ```ini
+      #{name} #{format_args(args)}
+      ```
+      #{if is_bitstring(doc), do: doc, else: "Seems like I didn't write any docs for this yet."}
+      """,
       color: Constants.color_blue()
     }
   end
@@ -30,38 +98,32 @@ defmodule Bolt.Cogs.Help do
     }
   end
 
-  @spec command_names() :: Enumerable.t()
-  defp command_names do
-    Cogs.all_commands()
-    |> Stream.map(fn {name, _} -> "`#{name}`" end)
-  end
+  @doc """
+  View all available commands.
 
+  If you want to view more detailed help for a single
+  command, use `#{Application.get_env(:bolt, :prefix)}help command` - for example, to show
+  what you're reading currently, use `#{Application.get_env(:bolt, :prefix)}help help`.
+  """
   Cogs.def help do
-    %Embed{
-      title: "Bolt - Commands",
-      description: command_names() |> Enum.join(", "),
-      color: Constants.color_blue()
-    }
-    |> Embed.footer(text: "Maybe if you bother me enough, I will add command descriptions.")
+    format_command_overview()
     |> Embed.send()
   end
 
-  # Cogs.set_parser(:help, fn args -> args |> String.downcase |> List.wrap end)
-  ## Using a custom parser with multiple commands
-  ## but different arity seems to break command parsing.
-  ## Maybe a bug with Alchemy?
+  @doc """
+  View detailed help for the given command.
+  """
   Cogs.def help(command_name) do
     command_name = String.downcase(command_name)
 
-    res =
-      case get_command_information(command_name) do
-        {module, arity, function_name} ->
-          format_command_help(module, arity, function_name)
-          |> Embed.send()
+    case command_information(command_name) do
+      {module, arity, function_name} ->
+        format_command_detail(module, arity, function_name)
+        |> Embed.send()
 
-        nil ->
-          format_command_not_found(command_name)
-          |> Embed.send()
-      end
+      nil ->
+        format_command_not_found(command_name)
+        |> Embed.send()
+    end
   end
 end
