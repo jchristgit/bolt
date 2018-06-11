c9063d9830185a4d8a8fd503cb603a39a770c949
Johannes Christ <johannes.christ@gmail.com>
2018-06-11 15:08:47 +0200

    Add code formatting.

--- config/config.exs
+++ config/config.exs
@@ -8,7 +8,6 @@ config :bolt,
 
 config :porcelain, driver: Porcelain.Driver.Basic
 
-
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- lib/bolt/cogs/hello.ex
+++ lib/bolt/cogs/hello.ex
@@ -2,7 +2,6 @@ defmodule Bolt.Cogs.Hello do
   use Alchemy.Cogs
 
   Cogs.def echo(content) do
-    Cogs.say "beep bop `#{content}`"
+    Cogs.say("beep bop `#{content}`")
   end
 end
-
