da88bf1f5bb44e6ebdb52070fbcdb5b66a545477
Johannes Christ <johannes.christ@gmail.com>
2018-06-14 16:29:18 +0200

    Remove unused `@handlers` attribute.

--- lib/bolt/consumer.ex
+++ lib/bolt/consumer.ex
@@ -3,14 +3,6 @@ defmodule Bolt.Consumer do
   alias Bolt.Cogs
   alias Bolt.Commander
 
-  @handlers %{
-    MESSAGE_CREATE: [
-      Cogs.Echo,
-      Cogs.GuildInfo,
-      Cogs.MemberInfo,
-      Cogs.RoleInfo
-    ]
-  }
 
   def start_link() do
     Consumer.start_link(__MODULE__)
