249b4e8236a5e384016e47bb8489241000a894c8
Johannes Christ <johannes.christ@gmail.com>
2018-06-16 19:37:56 +0200

    Remove unused `Helpers.avatar_url/1`.

--- lib/bolt/helpers.ex
+++ lib/bolt/helpers.ex
@@ -1,22 +1,7 @@
 defmodule Bolt.Helpers do
   alias Nostrum.Api
-  alias Nostrum.Struct.User
   alias Nostrum.Cache.GuildCache
   use Timex
-
-  @doc """
-  Returns the Avatar URL for the given user.
-  If the user does not have any avatar hash,
-  the default avatar for the discriminator is
-  returned instead.
-  """
-  @spec avatar_url(User.t()) :: String.t()
-  def avatar_url(user) do
-    case user.avatar do
-      nil -> "https://cdn.discordapp.com/embed/avatars/#{rem(user.discriminator, 5)}.png"
-      hash -> "https://cdn.discordapp.com/avatars/#{user.id}/#{hash}.png"
-    end
-  end
 
   @doc """
   Convert a boolean value to the
