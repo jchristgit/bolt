f3947b9ff7dfcc9dac04705bd95b1163c8c39a42
Johannes Christ <johannes.christ@gmail.com>
2018-06-16 19:51:14 +0200

    Add a test for `Helpers.bool_to_human/1`.

--- test/bolt/helpers_test.exs
+++ test/bolt/helpers_test.exs
@@ -1,12 +1,26 @@
 defmodule BoltTest.Helpers do
   use ExUnit.Case, async: true
 
+  describe "Helpers.bool_to_human/1" do
+    alias Bolt.Helpers
+
+    test "translates bools to yes / no" do
+      assert Helpers.bool_to_human(true) == "yes"
+      assert Helpers.bool_to_human(false) == "no"
+    end
+  end
+
   describe "Helpers.clean_content/1" do
     alias Bolt.Helpers
 
     test "properly escapes `@everyone` and `@here` mentions" do
       assert Helpers.clean_content("@everyone") == "@\u200Beveryone"
       assert Helpers.clean_content("@here") == "@\u200Bhere"
+    end
+
+    test "ignores regular user mentions" do
+      assert Helpers.clean_content("<@1920312310>") == "<@1920312310>"
+      assert Helpers.clean_content("<@1>") == "<@1>"
     end
   end
 end
