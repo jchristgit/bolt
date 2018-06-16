f927d4fceb8443d83509b32156a1e9f426f330a2
Johannes Christ <johannes.christ@gmail.com>
2018-06-16 19:58:03 +0200

    Add a test for `Parsers.join/1`.

--- test/bolt/commander/parsers.exs
+++ test/bolt/commander/parsers.exs
@@ -10,4 +10,21 @@ defmodule BoltTest.Commander.Parsers do
       assert Parsers.passthrough(["arg", "u", "ments"]) == ["arg", "u", "ments"]
     end
   end
+
+  describe "Parsers.join/1" do
+    alias Bolt.Commander.Parsers
+
+    test "joins arguments by spaces" do
+      assert Parsers.join(["hello", "world"]) == "hello world"
+      assert Parsers.join(["a", "b", "c"]) == "a b c"
+    end
+
+    test "returns first element if there is only one argument" do
+      assert Parsers.join(["test"]) == "test"
+    end
+
+    test "returns an empty string with no arguments given" do
+      assert Parsers.join([]) == ""
+    end
+  end
 end
