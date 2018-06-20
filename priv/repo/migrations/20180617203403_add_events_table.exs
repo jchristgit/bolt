4603a20ddcc5c23eb5ea3646e00e4cbc309386bd
Johannes Christ <johannes.christ@gmail.com>
2018-06-20 18:11:25 +0200

    Include migrations in formatter.

--- priv/repo/migrations/20180617203403_add_events_table.exs
+++ priv/repo/migrations/20180617203403_add_events_table.exs
@@ -3,9 +3,9 @@ defmodule Bolt.Repo.Migrations.AddEventsTable do
 
   def change do
     create table("events", comment: "Time-based events") do
-      add :timestamp,   :utc_datetime,  comment: "Timestamp at which the event should be executed"
-      add :event,       :string,        comment: "Event type, chosen from enum"
-      add :data,        :map,           comment: "Additional metadata for the event"
+      add(:timestamp, :utc_datetime, comment: "Timestamp at which the event should be executed")
+      add(:event, :string, comment: "Event type, chosen from enum")
+      add(:data, :map, comment: "Additional metadata for the event")
     end
   end
 end
