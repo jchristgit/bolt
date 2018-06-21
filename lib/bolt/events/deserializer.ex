727c2c66149fb7ef4eadc19a0889e95140d6cdef
Johannes Christ <johannes.christ@gmail.com>
2018-06-21 16:47:57 +0200

    Add the `UNBAN_MEMBER` event type.

--- lib/bolt/events/deserializer.ex
+++ lib/bolt/events/deserializer.ex
@@ -4,7 +4,8 @@ defmodule Bolt.Events.Deserializer do
   def valid_events() do
     [
       "CREATE_MESSAGE",
-      "REMOVE_ROLE"
+      "REMOVE_ROLE",
+      "UNBAN_MEMBER"
     ]
   end
 
@@ -14,6 +15,7 @@ defmodule Bolt.Events.Deserializer do
       }) do
     func = fn ->
       alias Nostrum.Api
+
       Api.create_message(channel_id, content)
     end
 
@@ -33,6 +35,21 @@ defmodule Bolt.Events.Deserializer do
            {:ok} <- Api.modify_guild_member(guild_id, user_id, roles: roles) do
         :ok
       end
+    end
+
+    {:ok, func}
+  end
+
+  def deserialize(%Event{
+        event: "UNBAN_MEMBER",
+        data: %{"guild_id" => guild_id, "user_id" => user_id}
+      }) do
+    func = fn ->
+      alias Nostrum.Api
+
+      IO.inspect guild_id, label: "guild"
+      IO.inspect user_id, label: "user"
+      {:ok} = Api.remove_guild_ban(guild_id, user_id)
     end
 
     {:ok, func}
