40e3c2e9cb620994a2ccd9058a61314f388484fe
Johannes Christ <johannes.christ@gmail.com>
2018-06-25 22:31:53 +0200

    Modlog a few basic events.

--- lib/bolt/consumer.ex
+++ lib/bolt/consumer.ex
@@ -2,8 +2,12 @@ defmodule Bolt.Consumer do
   @moduledoc "Consumes events sent by the API gateway."
 
   alias Bolt.Commander
+  alias Bolt.Helpers
+  alias Bolt.ModLog
   alias Bolt.Repo
   alias Bolt.Schema.SelfAssignableRoles
+  alias Nostrum.Struct.Snowflake
+  alias Nostrum.Struct.User
   use Nostrum.Consumer
 
   @spec start_link :: Supervisor.on_start()
@@ -15,6 +19,61 @@ defmodule Bolt.Consumer do
   @spec handle_event(Nostrum.Consumer.event()) :: any()
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
     Commander.handle_message(msg)
+  end
+
+  def handle_event(
+        {:MESSAGE_UPDATE,
+         {%{
+            content: content,
+            guild_id: guild_id,
+            author: author,
+            channel_id: channel_id,
+            id: msg_id
+          }}, _ws_state}
+      ) do
+    if content != "" do
+      jump_link = "https://discordapp.com/channels/#{guild_id}/#{channel_id}/#{msg_id}"
+
+      ModLog.emit(
+        guild_id,
+        "MESSAGE_EDIT",
+        """
+        #{author.username}##{author.discriminator} (`#{author.id}`) edited their message (<#{
+          jump_link
+        }>) to:
+        #{Helpers.clean_content(String.slice(content, 1..1800))}
+        """
+      )
+    end
+  end
+
+  def handle_event(
+        {:MESSAGE_DELETE, {%{channel_id: channel_id, guild_id: guild_id, id: msg_id}}, _ws_state}
+      ) do
+    ModLog.emit(
+      guild_id,
+      "MESSAGE_DELETE",
+      "message with ID `#{msg_id}` deleted in <##{channel_id}>"
+    )
+  end
+
+  def handle_event({:GUILD_MEMBER_ADD, {guild_id, member}, _ws_state}) do
+    creation_datetime = Snowflake.creation_time(member.user.id)
+
+    ModLog.emit(
+      guild_id,
+      "GUILD_MEMBER_ADD",
+      "#{User.full_name(member.user)} (`#{member.user.id}`) has joined " <>
+        "- account created #{Helpers.datetime_to_human(creation_datetime)}"
+    )
+  end
+
+  def handle_event({:GUILD_MEMBER_REMOVE, {guild_id, member}, _ws_state}) do
+    ModLog.emit(
+      guild_id,
+      "GUILD_MEMBER_REMOVE",
+      "#{User.full_name(member.user)} (`#{member.user.id}`) has left"
+    )
   end
 
   def handle_event({:MESSAGE_REACTION_ADD, {reaction}, _ws_state}) do
