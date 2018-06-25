8b9648cfe7a942d5e313d03c3af24b2e023fa35c
Johannes Christ <johannes.christ@gmail.com>
2018-06-25 17:15:46 +0200

    Add `ExCoveralls`.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Bolt.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test]
     ]
   end
 
@@ -28,7 +30,8 @@ defmodule Bolt.MixProject do
       {:postgrex, "~> 0.13.5"},
       {:timex, "~> 3.1"},
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.2", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.8", only: :test, runtime: false}
     ]
   end
 end
