e4216b2e691c48d12fa00569cc1de176d12b60fa
Johannes Christ <johannes.christ@gmail.com>
2018-06-25 21:47:11 +0200

    Paginate `modlog status` output.

--- lib/bolt/cogs/modlog/status.ex
+++ lib/bolt/cogs/modlog/status.ex
@@ -1,39 +1,51 @@
 defmodule Bolt.Cogs.ModLog.Status do
   @moduledoc false
 
+  alias Bolt.Constants
   alias Bolt.Repo
   alias Bolt.Schema.ModLogConfig
+  alias Bolt.Paginator
   import Ecto.Query, only: [from: 2]
   alias Nostrum.Api
+  alias Nostrum.Struct.Embed
 
   @spec command(Nostrum.Struct.Message.t(), [String.t()]) :: {:ok, Nostrum.Struct.Message.t()}
   def command(msg, []) do
     query = from(config in ModLogConfig, where: config.guild_id == ^msg.guild_id, select: config)
 
-    response =
-      case Repo.all(query) do
-        [] ->
-          "❌ modlog is not configured"
+    case Repo.all(query) do
+      [] ->
+        response = "❌ modlog is not configured"
+        {:ok, _msg} = Api.create_message(msg.channel_id, response)
 
-        config_rows ->
-          event_log_descriptions =
-            config_rows
-            |> Enum.group_by(
-              fn row -> row.event end,
-              fn row -> row.channel_id end
-            )
-            |> Map.to_list()
-            |> Stream.map(fn {event, channels} ->
-              {event, Stream.map(channels, &"<##{&1}>")}
-            end)
-            |> Enum.map(fn {event, channels} ->
-              "• `#{event}` is logged in #{Enum.join(channels, ", ")}"
-            end)
+      config_rows ->
+        pages =
+          config_rows
+          |> Enum.group_by(
+            fn row -> row.event end,
+            fn row -> row.channel_id end
+          )
+          |> Map.to_list()
+          |> Stream.map(fn {event, channels} ->
+            {event, Stream.map(channels, &"<##{&1}>")}
+          end)
+          |> Stream.map(fn {event, channels} ->
+            "• `#{event}` is logged in #{Enum.join(channels, ", ")}"
+          end)
+          |> Stream.chunk_every(6)
+          |> Enum.map(fn entry_chunk ->
+            %Embed{
+              description: Enum.join(entry_chunk, "\n")
+            }
+          end)
 
-          "**Mod log configuration**\n#{event_log_descriptions}"
-      end
+        base_embed = %Embed{
+          title: "Mod log configuration",
+          color: Constants.color_blue()
+        }
 
-    {:ok, _msg} = Api.create_message(msg.channel_id, response)
+        Paginator.paginate_over(msg, base_embed, pages)
+    end
   end
 
   def command(msg, _args) do
