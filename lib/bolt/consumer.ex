2540f45e718b73f3fcb4d3f78079f3861ca4676b
Johannes Christ <johannes.christ@gmail.com>
2018-06-25 18:05:51 +0200

    Remove self-assignable roles on deletion.

--- lib/bolt/consumer.ex
+++ lib/bolt/consumer.ex
@@ -2,6 +2,8 @@ defmodule Bolt.Consumer do
   @moduledoc "Consumes events sent by the API gateway."
 
   alias Bolt.Commander
+  alias Bolt.Repo
+  alias Bolt.Schema.SelfAssignableRoles
   use Nostrum.Consumer
 
   @spec start_link :: Supervisor.on_start()
@@ -17,6 +19,22 @@ defmodule Bolt.Consumer do
 
   def handle_event({:MESSAGE_REACTION_ADD, {reaction}, _ws_state}) do
     GenServer.cast(Bolt.Paginator, {:MESSAGE_REACTION_ADD, reaction})
+  end
+
+  def handle_event({:GUILD_ROLE_DELETE, {guild_id, deleted_role}, _ws_state}) do
+    case Repo.get(SelfAssignableRoles, guild_id) do
+      %SelfAssignableRoles{roles: role_list} = sar_row ->
+        if deleted_role.id in role_list do
+          updated_roles = Enum.reject(role_list, & &1 == deleted_role.id)
+          changeset = SelfAssignableRoles.changeset(sar_row, %{roles: updated_roles})
+          Repo.update(changeset)
+        else
+          :noop
+        end
+
+      _ ->
+        :noop
+    end
   end
 
   @impl true
