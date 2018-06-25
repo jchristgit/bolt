ad1b9e1a4218075a0a8552fbb4fe0ceeb2ab9cc5
Johannes Christ <johannes.christ@gmail.com>
2018-06-25 19:47:56 +0200

    Add a migration for the `ModLogConfig` table.

---
+++ priv/repo/migrations/20180625173940_create_mod_log_config_table.exs
@@ -0,0 +1,33 @@
+defmodule Bolt.Repo.Migrations.CreateModLogConfigTable do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      """
+      CREATE TYPE modlog_event AS ENUM (
+        'AUTOMOD', 'BOT_UPDATE', 'CONFIG_UPDATE',
+        'INFRACTION_CREATE', 'INFRACTION_UPDATE', 'INFRACTION_EVENTS',
+
+        'CHANNEL_CREATE', 'CHANNEL_UPDATE', 'CHANNEL_DELETE',
+        'MESSAGE_EDIT', 'MESSAGE_DELETE',
+        'GUILD_MEMBER_ADD', 'GUILD_MEMBER_UPDATE', 'GUILD_MEMBER_REMOVE',
+        'GUILD_ROLE_CREATE', 'GUILD_ROLE_UPDATE', 'GUILD_ROLE_DELETE',
+        'USER_UPDATE'
+      );
+      """,
+      "DROP TYPE modlog_event;"
+    )
+
+    create table("modlogconfig", primary_key: false, comment: "Moderation log configuration") do
+      add :guild_id, :bigint, primary_key: true, comment: "The guild ID this row applies to"
+      add :event, :modlog_event, null: false, comment: "The event that is configured by this row"
+      add :channel_id, :bigint, null: false, comment: "The channel ID to log this event in"
+    end
+
+    create index(
+      "modlogconfig",
+      [:guild_id, :event],
+      unique: true
+    )
+  end
+end
