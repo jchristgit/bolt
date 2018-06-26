251ac660f985e0a9dfb3cd0e3032a259b99fa405
Johannes Christ <johannes.christ@gmail.com>
2018-06-26 12:23:38 +0200

    Wire up the new cogs.

--- lib/bolt/cogs/sudo.ex
+++ lib/bolt/cogs/sudo.ex
@@ -1,7 +1,35 @@
 defmodule Bolt.Cogs.Sudo do
+  @moduledoc false
+
+  alias Nostrum.Api
+
+  @spec command(Nostrum.Struct.Message.t(), [String.t()]) :: {:ok, Nostrum.Struct.Message.t()}
   def command(msg, ["guilds" | args]) do
     alias Bolt.Cogs.Sudo.Guilds
 
     Guilds.command(msg, args)
+  end
+
+  def command(msg, ["leaveguild" | args]) do
+    alias Bolt.Cogs.Sudo.LeaveGuild
+
+    LeaveGuild.command(msg, args)
+  end
+
+  def command(msg, ["send" | args]) do
+    alias Bolt.Cogs.Sudo.Send
+
+    Send.command(msg, args)
+  end
+
+  def command(msg, ["log" | args]) do
+    alias Bolt.Cogs.Sudo.Log
+
+    Log.command(msg, args)
+  end
+
+  def command(msg, _args) do
+    response = "🚫 unknown subcommand"
+    {:ok, _msg} = Api.create_message(msg.channel_id, response)
   end
 end
