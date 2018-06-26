fa2146106d49b1b37e3a71766dd11f1d460f5406
Johannes Christ <johannes.christ@gmail.com>
2018-06-26 12:22:59 +0200

    Add a sudo command to leave guilds.

---
+++ lib/bolt/cogs/sudo/leave_guild.ex
@@ -0,0 +1,25 @@
+defmodule Bolt.Cogs.Sudo.LeaveGuild do
+  @moduledoc false
+
+  alias Nostrum.Api
+  alias Bolt.Helpers
+
+  @spec command(Nostrum.Struct.Message.t(), [String.t()]) :: {:ok, Nostrum.Struct.Message.t()}
+  def command(msg, [guild_id]) do
+    response = case Integer.parse(guild_id) do
+      {value, _rest} ->
+        {:ok} = Api.leave_guild(value)
+        "👌 left guild `#{value}`"
+
+      :error ->
+        "🚫 `#{Helpers.clean_content(guild_id)}` is not a valid guild ID"
+    end
+
+    {:ok, _msg} = Api.create_message(msg.channel_id, response)
+  end
+
+  def command(msg, _args) do
+    response = "🚫 this command only takes one argument, the guild ID to leave"
+    {:ok, _msg} = Api.create_message(msg.channel_id, response)
+  end
+end
