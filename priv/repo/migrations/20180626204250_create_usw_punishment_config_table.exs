2bc4dd022309284e810793df4b10aea826638432
Johannes Christ <johannes.christ@gmail.com>
2018-06-27 14:22:59 +0200

    Format migration nicely.

--- priv/repo/migrations/20180626204250_create_usw_punishment_config_table.exs
+++ priv/repo/migrations/20180626204250_create_usw_punishment_config_table.exs
@@ -7,11 +7,34 @@ defmodule Bolt.Repo.Migrations.CreateUswPunishmentConfigTable do
       "DROP TYPE punishment_type;"
     )
 
-    create table("usw_punishment_config", primary_key: false, comment: "Configuration for the USW anti-spam module") do
-      add :guild_id, :bigint, primary_key: true, comment: "The guild ID this configuration row applies to"
-      add :duration, :integer, comment: "The duration (in seconds) that the punishment will last"
-      add :punishment, :punishment_type, nullable: false, comment: "The configured punishment type for automod actions"
-      add :data, :map, nullable: false, default: %{}, comment: "Additional metadata used with the given punishment type"
+    create table(
+             "usw_punishment_config",
+             primary_key: false,
+             comment: "Configuration for the USW anti-spam module"
+           ) do
+      add(
+        :guild_id,
+        :bigint,
+        primary_key: true,
+        comment: "The guild ID this configuration row applies to"
+      )
+
+      add(:duration, :integer, comment: "The duration (in seconds) that the punishment will last")
+
+      add(
+        :punishment,
+        :punishment_type,
+        nullable: false,
+        comment: "The configured punishment type for automod actions"
+      )
+
+      add(
+        :data,
+        :map,
+        nullable: false,
+        default: %{},
+        comment: "Additional metadata used with the given punishment type"
+      )
     end
   end
 end
