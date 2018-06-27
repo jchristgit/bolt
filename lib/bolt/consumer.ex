1326723b2ec4b9a4dd2ff2667ad61beac44b70e1
Johannes Christ <johannes.christ@gmail.com>
2018-06-27 15:09:12 +0200

    Remove debug prints.

--- lib/bolt/consumer.ex
+++ lib/bolt/consumer.ex
@@ -21,9 +21,6 @@ defmodule Bolt.Consumer do
   @impl true
   @spec handle_event(Nostrum.Consumer.event()) :: any()
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
-    IO.inspect(msg.content, label: "content")
-    IO.inspect(msg.author.bot, label: "is bot")
-
     unless msg.author.bot do
       Commander.handle_message(msg)
       MessageCache.consume(msg)
