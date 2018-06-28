38f8cf4ba1bc6b81d042033e899fe936e3ac4a9b
Johannes Christ <johannes.christ@gmail.com>
2018-06-28 20:20:27 +0200

    Remove unused `id` column on `selfassignableroles`.

---
+++ priv/repo/migrations/20180628181701_remove_selfassignableroles_id.exs
@@ -0,0 +1,15 @@
+defmodule Bolt.Repo.Migrations.RemoveSelfassignablerolesId do
+  use Ecto.Migration
+
+  def up do
+    alter table("selfassignableroles") do
+      remove :id
+    end
+  end
+
+  def down do
+    alter table("selfassignableroles") do
+      add :id, :bigint, primary_key: true
+    end
+  end
+end
