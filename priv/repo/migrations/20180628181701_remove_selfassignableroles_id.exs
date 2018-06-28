641f3c6e9af2fad8967c5307dfa1be8c6d33ac57
Johannes Christ <johannes.christ@gmail.com>
2018-06-28 22:55:20 +0200

    Use parantheses with function calls.

--- priv/repo/migrations/20180628181701_remove_selfassignableroles_id.exs
+++ priv/repo/migrations/20180628181701_remove_selfassignableroles_id.exs
@@ -3,13 +3,13 @@ defmodule Bolt.Repo.Migrations.RemoveSelfassignablerolesId do
 
   def up do
     alter table("selfassignableroles") do
-      remove :id
+      remove(:id)
     end
   end
 
   def down do
     alter table("selfassignableroles") do
-      add :id, :bigint, primary_key: true
+      add(:id, :bigint, primary_key: true)
     end
   end
 end
