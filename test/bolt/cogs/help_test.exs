8a7bb6ea02915e7691c9a08c9f2e60ad637cb8cb
Johannes Christ <johannes.christ@gmail.com>
2018-06-29 21:08:00 +0200

    Remove a broken test.

--- test/bolt/cogs/help_test.exs
+++ test/bolt/cogs/help_test.exs
@@ -3,22 +3,11 @@ defmodule BoltTest.Cogs.Help do
 
   describe "format_command_detail/1" do
     alias Bolt.Cogs.Help
+    alias Nostrum.Struct.Embed
 
     test "returns an embed" do
-      alias Nostrum.Struct.Embed
-
       assert %Embed{} =
                Help.format_command_detail("test command", ["test usage"], "test description")
-    end
-
-    test "inserts prefix in front of usage lines" do
-      alias Nostrum.Struct.Embed
-
-      %Embed{description: content} =
-        Help.format_command_detail("test command", ["test usage"], "test description")
-
-      {:ok, prefix} = Application.fetch_env(:bolt, :prefix)
-      assert String.contains?(content, "#{prefix}test usage")
     end
   end
 end
