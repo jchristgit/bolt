008d50dbad0b57b0c35be863389b19369b8af8c5
Johannes Christ <johannes.christ@gmail.com>
2018-07-14 01:14:41 +0200

    Properly distribute `sudo log`.

--- lib/bolt/cogs/sudo/log.ex
+++ lib/bolt/cogs/sudo/log.ex
@@ -2,7 +2,10 @@ defmodule Bolt.Cogs.Sudo.Log do
   @moduledoc false
 
   alias Bolt.ModLog
+  alias Bolt.Repo
+  alias Bolt.Schema.ModLogConfig
   alias Nostrum.Api
+  import Ecto.Query, only: [from: 2]
 
   @spec command(Nostrum.Struct.Message.t(), [String.t()]) :: {:ok, Nostrum.Struct.Message.t()}
   def command(msg, content_list) when content_list != [] do
@@ -10,11 +13,25 @@ defmodule Bolt.Cogs.Sudo.Log do
     startup_note = "⏲ distributing event, please wait"
     {:ok, startup_msg} = Api.create_message(msg.channel_id, startup_note)
 
-    ModLog.emit(
-      msg.guild_id,
-      "BOT_UPDATE",
-      content
-    )
+    query =
+      from(
+        conf in ModLogConfig,
+        where: conf.event == "BOT_UPDATE",
+        distinct: conf.guild_id,
+        select: conf.guild_id
+      )
+
+    query
+    |> Repo.all()
+    |> Enum.map(fn guild_id ->
+      ModLog.emit(
+        guild_id,
+        "BOT_UPDATE",
+        content
+      )
+
+      :timer.sleep(200)
+    end)
 
     response = "👌 event broadcasted to subscribed guilds"
     {:ok, _msg} = Api.create_message(msg.channel_id, response)
