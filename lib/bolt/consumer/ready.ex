170ac11b9866e41ec2a49db8c4ad017182f0d7fe
Johannes Christ <johannes.christ@gmail.com>
2018-07-13 20:48:34 +0200

    Show "watching" status with `.help` command.

--- lib/bolt/consumer/ready.ex
+++ lib/bolt/consumer/ready.ex
@@ -2,9 +2,11 @@ defmodule Bolt.Consumer.Ready do
   @moduledoc "Handles the `READY` event."
 
   alias Bolt.BotLog
+  alias Nostrum.Api
 
   @spec handle(map()) :: :ok
   def handle(data) do
     BotLog.emit("⚡ Logged in and ready, seeing `#{length(data.guilds)}` guilds.")
+    :ok = Api.update_status(:online, "you | .help", 3)
   end
 end
