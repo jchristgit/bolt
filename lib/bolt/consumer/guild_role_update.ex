22128df30589d7d2328bf9efe31d4acc046a5c59
Johannes Christ <johannes.christ@gmail.com>
2018-07-19 00:32:59 +0200

    Properly log `GUILD_ROLE_UPDATE`.

--- lib/bolt/consumer/guild_role_update.ex
+++ lib/bolt/consumer/guild_role_update.ex
@@ -1,28 +1,31 @@
 defmodule Bolt.Consumer.GuildRoleUpdate do
   @moduledoc "Handles the `GUILD_ROLE_CREATE` event."
 
-  alias Bolt.ModLog
+  alias Bolt.{Helpers, ModLog}
   alias Nostrum.Struct.Guild
   alias Nostrum.Struct.Guild.Role
   alias Nostrum.Struct.Permission
 
   @spec handle(Guild.id(), Role.t(), Role.t()) :: ModLog.on_emit()
   def handle(guild_id, old_role, new_role) do
-    diff_string = []
-                |> add_if_different(old_role, new_role, :color)
-                |> add_if_different(old_role, new_role, :hoist)
-                |> add_if_different(old_role, new_role, :managed)
-                |> add_if_different(old_role, new_role, :mentionable)
-                |> add_if_different(old_role, new_role, :name)
-                |> add_if_different(old_role, new_role, :permissions)
-                |> add_if_different(old_role, new_role, :position)
-                |> Enum.join(", ")
+    diff_string =
+      []
+      |> add_if_different(old_role, new_role, :color)
+      |> add_if_different(old_role, new_role, :hoist)
+      |> add_if_different(old_role, new_role, :managed)
+      |> add_if_different(old_role, new_role, :mentionable)
+      |> add_if_different(old_role, new_role, :name)
+      |> add_if_different(old_role, new_role, :permissions)
+      |> add_if_different(old_role, new_role, :position)
+      |> Enum.join(", ")
 
-      ModLog.emit(
-        guild_id,
-        "GUILD_ROLE_UPDATE",
-        "role `#{old_role.name}` was updated: #{diff_string}"
+    ModLog.emit(
+      guild_id,
+      "GUILD_ROLE_UPDATE",
+      Helpers.clean_content(
+        "role #{old_role.name} (`#{old_role.id}`) #{diff_string}"
       )
+    )
   end
 
   @spec add_if_different([String.t()], Role.t(), Role.t(), atom()) :: [String.t()]
@@ -34,13 +37,15 @@ defmodule Bolt.Consumer.GuildRoleUpdate do
     added_perms = difference |> Keyword.get_values(:ins) |> List.flatten()
     removed_perms = difference |> Keyword.get_values(:del) |> List.flatten()
 
-    diff_list ++ Enum.map(
-      added_perms,
-      &"added permission `#{&1}`"
-    ) ++ Enum.map(
-      removed_perms,
-      &"removed permission `#{&1}`"
-    )
+    diff_list ++
+      Enum.map(
+        added_perms,
+        &"added permission `#{&1}`"
+      ) ++
+      Enum.map(
+        removed_perms,
+        &"removed permission `#{&1}`"
+      )
   end
 
   defp add_if_different(diff_list, old_role, new_role, key) do
@@ -49,9 +54,19 @@ defmodule Bolt.Consumer.GuildRoleUpdate do
 
     if old_value != new_value do
       cond do
-        new_value === true -> diff_list ++ ["now #{key}"]
-        new_value === false -> diff_list ++ ["no longer #{key}"]
-        true -> diff_list ++ ["#{key |> Atom.to_string() |> String.capitalize()} updated from `#{old_value}` to `#{new_value}`"]
+        new_value === true ->
+          diff_list ++ ["now #{key}"]
+
+        new_value === false ->
+          diff_list ++ ["no longer #{key}"]
+
+        true ->
+          diff_list ++
+            [
+              "#{key |> Atom.to_string() |> String.capitalize()} updated from `#{old_value}` to `#{
+                new_value
+              }`"
+            ]
       end
     else
       diff_list
