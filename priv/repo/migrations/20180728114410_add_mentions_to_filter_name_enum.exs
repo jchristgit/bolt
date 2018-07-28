ec2ea0e2f09de045e535d976632e682f19bb91c9
Johannes Christ <johannes.christ@gmail.com>
2018-07-28 15:31:47 +0200

    Remove duplicate migration.

--- priv/repo/migrations/20180728114410_add_mentions_to_filter_name_enum.exs
+++
@@ -1,8 +0,0 @@
-defmodule Bolt.Repo.Migrations.AddMentionsToFilterNameEnum do
-  use Ecto.Migration
-  @disable_ddl_transaction true
-
-  def up do
-    execute("ALTER TYPE filter_name ADD VALUE 'MENTIONS';")
-  end
-end
