6ab9e157a2bcb2d254d8e0c55ea317f5d2886f4b
Johannes Christ <jc@jchri.st>
2019-04-06 20:18:37 +0200

    `exec` into mix process to drop lingering shell process.

--- docker/migrate_and_boot.sh
+++ docker/migrate_and_boot.sh
@@ -2,12 +2,12 @@
 
 # Maximum amount of seconds to wait for the PostgreSQL container
 # to accept our connection before exiting.
-timeout=3
+timeout=5
 
 # Interval at which the PostgreSQL container should be checked for availability.
 interval=1
 
-max_retries=$(expr $timeout \* $interval)
+max_retries=$(expr ${timeout} \* ${interval})
 attempt=0
 
 function ping_postgres() {
@@ -17,18 +17,18 @@ function ping_postgres() {
 
 # Wait for the PostgreSQL container to be accessible.
 while [[ ! ping_postgres ]]; do
-    if [[ $attempt -eq $max_retries ]]; then
+    if [[ ${attempt} -eq ${max_retries} ]]; then
         echo [!] Unable to access PostgreSQL after maximum
-        echo [!] specified timeout of $timeout seconds.
+        echo [!] specified timeout of ${timeout} seconds.
         exit 1
     fi
 
     echo [i] Waiting for the PostgreSQL to boot,
-    echo [i] attempt $attempt/$max_retries. Sleeping for $interval s.
-    sleep $interval
+    echo [i] attempt ${attempt}/${max_retries}. Sleeping for ${interval}s.
+    sleep ${interval}
     let attempt+=1
 done
 
 # Ensure our schema is up-to-date, and start bolt.
-mix do ecto.migrate --quiet
-mix run --no-halt
+mix ecto.migrate --quiet
+exec mix run --no-halt
