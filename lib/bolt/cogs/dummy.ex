20382ff347f5a9c96ce82bd805bdf5382b1d85f0
Johannes Christ <jc@jchri.st>
2019-05-14 22:16:26 +0200

    Remove the `Dummy` cog.

--- lib/bolt/cogs/dummy.ex
+++
@@ -1,7 +0,0 @@
-defmodule Bolt.Cogs.Dummy do
-  @moduledoc "A dummy cog, useful when a callback is required but not wanted."
-
-  @spec command(Nostrum.Struct.Message.t(), [String.t()]) :: no_return()
-  def command(_msg, _args) do
-  end
-end
