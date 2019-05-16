f586859dd77c39f1426ffd7d5183b3cfa1684a76
Johannes Christ <jc@jchri.st>
2019-05-16 22:22:53 +0200

    Load the `graphs` command.

--- lib/bolt/consumer/ready.ex
+++ lib/bolt/consumer/ready.ex
@@ -17,6 +17,7 @@ defmodule Bolt.Consumer.Ready do
     "memberinfo" => Cogs.MemberInfo,
     "roleinfo" => Cogs.RoleInfo,
     "roles" => Cogs.Roles,
+    "graphs" => Cogs.Graphs,
 
     ## Self-assignable roles
     "lsar" => Cogs.Lsar,
