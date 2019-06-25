e329ac98dc618f0ccebed566568ce33cafc3df76
Johannes Christ <jc@jchri.st>
2019-06-25 23:23:42 +0200

    Simplify message update consuming.

--- lib/bolt/consumer/message_update.ex
+++ lib/bolt/consumer/message_update.ex
@@ -6,54 +6,58 @@ defmodule Bolt.Consumer.MessageUpdate do
   alias Nostrum.Snowflake
   alias Nostrum.Struct.Embed
   alias Nostrum.Struct.Embed.{Author, Field}
+  alias Nostrum.Struct.Message
 
-  @spec handle(Nostrum.Struct.Message.t()) :: :ok
-  def handle(msg) do
-    unless Map.get(msg, :content, "") == "" do
-      from_cache = MessageCache.get(msg.guild_id, msg.id)
+  @spec handle(Nostrum.Struct.Message.t()) :: :ok | :ignored
+  def handle(%Message{content: content, guild_id: guild_id} = msg)
+      when content != "" and guild_id != nil do
+    from_cache = MessageCache.get(msg.guild_id, msg.id)
 
-      embed = %Embed{
-        author: %Author{
-          name: format_author(from_cache)
-          # Once the nostrum bug with users being sent as raw maps
-          # in the event payload is fixed, edit this back in, and change
-          # the user#discrim building above to User.full_name/1.
-          # icon_url: User.avatar_url(author)
+    embed = %Embed{
+      author: %Author{
+        name: format_author(from_cache)
+        # Once the nostrum bug with users being sent as raw maps
+        # in the event payload is fixed, edit this back in, and change
+        # the user#discrim building above to User.full_name/1.
+        # icon_url: User.avatar_url(author)
+      },
+      color: Constants.color_blue(),
+      url: "https://discordapp.com/channels/#{msg.guild_id}/#{msg.channel_id}/#{msg.id}",
+      fields: [
+        %Field{
+          name: "Metadata",
+          value: """
+          Channel: <##{msg.channel_id}>
+          Creation: #{msg.id |> Snowflake.creation_time() |> Helpers.datetime_to_human()}
+          Message ID: #{msg.id}
+          """
         },
-        color: Constants.color_blue(),
-        url: "https://discordapp.com/channels/#{msg.guild_id}/#{msg.channel_id}/#{msg.id}",
-        fields: [
-          %Field{
-            name: "Metadata",
-            value: """
-            Channel: <##{msg.channel_id}>
-            Creation: #{msg.id |> Snowflake.creation_time() |> Helpers.datetime_to_human()}
-            Message ID: #{msg.id}
-            """
-          },
-          %Field{
-            name: "Old content",
-            value:
-              (fn ->
-                 content =
-                   if(from_cache != nil, do: from_cache.content, else: "*unknown, not in cache*")
+        %Field{
+          name: "Old content",
+          value:
+            (fn ->
+               content =
+                 if(from_cache != nil, do: from_cache.content, else: "*unknown, not in cache*")
 
-                 String.slice(content, 0..1020)
-               end).(),
-            inline: true
-          },
-          %Field{
-            name: "Updated content",
-            value: String.slice(msg.content, 0..1020),
-            inline: true
-          }
-        ]
-      }
+               String.slice(content, 0..1020)
+             end).(),
+          inline: true
+        },
+        %Field{
+          name: "Updated content",
+          value: String.slice(msg.content, 0..1020),
+          inline: true
+        }
+      ]
+    }
 
-      ModLog.emit_embed(msg.guild_id, "MESSAGE_EDIT", embed)
+    ModLog.emit_embed(msg.guild_id, "MESSAGE_EDIT", embed)
 
-      MessageCache.update(msg)
-    end
+    MessageCache.update(msg)
+  end
+
+  def handle(_msg) do
+    :ignored
   end
 
   defp format_author(cached_message)
