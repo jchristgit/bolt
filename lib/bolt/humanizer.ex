6fa06c7f25bbcb8faeb66137128023a227b8d087
Johannes Christ <jc@jchri.st>
2019-06-26 23:05:26 +0200

    Add a function to humanize users.

--- lib/bolt/humanizer.ex
+++ lib/bolt/humanizer.ex
@@ -1,10 +1,12 @@
 defmodule Bolt.Humanizer do
   @moduledoc "Produces human-readable descriptions from snowflakes."
 
-  alias Nostrum.Cache.GuildCache
+  alias Nostrum.Cache.{GuildCache, UserCache}
   alias Nostrum.Struct.Guild
   alias Nostrum.Struct.Guild.Role
+  alias Nostrum.Struct.User
   import Nosedrum.Helpers, only: [escape_server_mentions: 1]
+  require Logger
 
   @doc "Humanize a role."
   @spec human_role(Guild.id(), Role.id()) :: String.t()
@@ -15,6 +17,18 @@ defmodule Bolt.Humanizer do
 
       _other ->
         "`#{role_id}`"
+    end
+  end
+
+  @doc "Humanize a user."
+  @spec human_user(User.id()) :: String.t()
+  def human_user(user_id) do
+    case UserCache.get(user_id) do
+      {:ok, user} ->
+        escape_server_mentions("#{User.full_name(user)} (`#{user_id}`)")
+
+      _other ->
+        "`#{user_id}`"
     end
   end
 end
--- test/bolt/humanizer_test.exs
+++ test/bolt/humanizer_test.exs
@@ -19,8 +19,8 @@ defmodule Bolt.HumanizerTest do
     setup do
       start_supervised!(Nostrum.Cache.CacheSupervisor)
 
-      guild_id = 219501
-      role_id = 1295015
+      guild_id = 219_501
+      role_id = 1_295_015
       role_name = "Robot with @everyone permissions"
 
       guild = %{
@@ -43,12 +43,55 @@ defmodule Bolt.HumanizerTest do
       assert "`-1`" = Humanizer.human_role(guild_id, -1)
     end
 
-    test "returns escaped role name on known roles", %{guild_id: guild_id, role_id: role_id, role_name: role_name} do
+    test "returns escaped role name on known roles", %{
+      guild_id: guild_id,
+      role_id: role_id,
+      role_name: role_name
+    } do
       expected_name = escape_server_mentions(role_name)
 
       refute expected_name =~ "@everyone"
       assert "#{expected_name} (`#{role_id}`)" == Humanizer.human_role(guild_id, role_id)
       refute Humanizer.human_role(guild_id, role_id) =~ "@everyone"
+    end
+  end
+
+  describe "human_user/1" do
+    setup do
+      # cleaned up at test process exit by the vm
+      :users = :ets.new(:users, [:set, :public, :named_table])
+      :ok
+    end
+
+    test "returns the user ID when not found" do
+      assert "`55`" = Humanizer.human_user(55)
+    end
+  end
+
+  describe "human_user/1 with cached user" do
+    setup do
+      :users = :ets.new(:users, [:set, :public, :named_table])
+
+      user = %{
+        discriminator: "4444",
+        id: 120_951,
+        username: "@everyone hi"
+      }
+
+      true = :ets.insert(:users, {user.id, user})
+
+      %{user: user}
+    end
+
+    test "returns escaped user name for known users", %{user: user} do
+      expected_name = escape_server_mentions(user.username)
+
+      refute expected_name =~ "@everyone"
+
+      assert "#{expected_name}##{user.discriminator} (`#{user.id}`)" ==
+               Humanizer.human_user(user.id)
+
+      refute Humanizer.human_user(user.id) =~ "@everyone"
     end
   end
 end
