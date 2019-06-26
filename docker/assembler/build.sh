0c7d8718ba5ef255b13441253d78dc291564d0ac
Johannes Christ <jc@jchri.st>
2019-06-26 23:24:05 +0200

    Use new namespaced path to the Distillery release task.

--- docker/assembler/build.sh
+++ docker/assembler/build.sh
@@ -20,6 +20,6 @@ mix do deps.get --only prod
 # Run an explicit clean to remove any build artifacts from the host
 mix do clean, compile --force
 # Build the release
-mix release
+mix distillery.release
 # Copy tarball to output
 cp "_build/prod/rel/$APP_NAME/releases/$APP_VSN/$APP_NAME.tar.gz" rel/artifacts/"$APP_NAME-$APP_VSN.tar.gz"
