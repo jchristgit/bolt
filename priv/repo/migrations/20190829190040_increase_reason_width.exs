5dbf29ac840ce5da2c144d1d1a958e7e02b93285
Johannes Christ <jc@jchri.st>
2019-11-25 21:36:50 +0100

    Format migration.

--- priv/repo/migrations/20190829190040_increase_reason_width.exs
+++ priv/repo/migrations/20190829190040_increase_reason_width.exs
@@ -3,7 +3,7 @@ defmodule Bolt.Repo.Migrations.IncreaseReasonWidth do
 
   def change do
     alter table("infractions") do
-      modify :reason, :"VARCHAR(2000)", from: :"VARCHAR(255)"
+      modify(:reason, :"VARCHAR(2000)", from: :"VARCHAR(255)")
     end
   end
 end
