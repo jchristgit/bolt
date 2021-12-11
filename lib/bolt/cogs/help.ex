0dc2611f35ce00ef12d3e8c1450f85e36f83675d
Johannes Christ <jc@jchri.st>
2021-12-11 14:14:04 +0100

    Implement refactoring suggestions

--- lib/bolt/cogs/help.ex
+++ lib/bolt/cogs/help.ex
@@ -110,24 +110,24 @@ defmodule Bolt.Cogs.Help do
   end
 
   def command(msg, [command_group, subcommand_name]) do
-    # credo:disable-for-next-line Credo.Check.Refactor.WithClauses
-    with command_map when is_map(command_map) <- CommandStorage.lookup_command(command_group) do
-      case Map.fetch(command_map, subcommand_name) do
-        {:ok, command_module} ->
-          embed = format_command_detail("#{command_group} #{subcommand_name}", command_module)
-          {:ok, _msg} = Api.create_message(msg.channel_id, embed: embed)
+    case CommandStorage.lookup_command(command_group) do
+      command_map when is_map(command_map) ->
+        case Map.fetch(command_map, subcommand_name) do
+          {:ok, command_module} ->
+            embed = format_command_detail("#{command_group} #{subcommand_name}", command_module)
+            {:ok, _msg} = Api.create_message(msg.channel_id, embed: embed)
 
-        :error ->
-          subcommand_string =
-            command_map |> Map.keys() |> Stream.map(&"`#{&1}`") |> Enum.join(", ")
+          :error ->
+            subcommand_string =
+              command_map |> Map.keys() |> Stream.map(&"`#{&1}`") |> Enum.join(", ")
 
-          response =
-            "🚫 unknown subcommand `#{Helpers.clean_content(subcommand_name)}`," <>
-              " known commands: #{subcommand_string}"
+            response =
+              "🚫 unknown subcommand `#{Helpers.clean_content(subcommand_name)}`," <>
+                " known commands: #{subcommand_string}"
 
-          {:ok, _msg} = Api.create_message(msg.channel_id, response)
-      end
-    else
+            {:ok, _msg} = Api.create_message(msg.channel_id, response)
+        end
+
       nil ->
         response = "🚫 no command group named `#{Helpers.clean_content(command_group)}` found"
         {:ok, _msg} = Api.create_message(msg.channel_id, response)
