0887963fbdede240b96c4dd5f31d7f676a865977
Johannes Christ <jc@jchri.st>
2022-02-13 23:13:25 +0100

    Revert "Remove stray `else`"
    This reverts commit 0e1f1c80a3268ec83df0abd955c949c97f3d98d8.

--- lib/bolt/action/delete_vanity_url.ex
+++ lib/bolt/action/delete_vanity_url.ex
@@ -39,6 +39,7 @@ defmodule Bolt.Action.DeleteVanityUrl do
       {:error, _reason} ->
         ModLog.emit(guild_id, "ERROR", "failed to delete vanity URLs due to Discord API error")
     end
+  else
   end
 
   defimpl String.Chars do
