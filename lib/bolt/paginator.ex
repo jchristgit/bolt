7ea8599e2faf0bcfdd595aa63fab9ce0165d1e21
Johannes Christ <jc@jchri.st>
2023-05-23 20:42:24 +0200

    Remove obsolete sleep for guide reactions

--- lib/bolt/paginator.ex
+++ lib/bolt/paginator.ex
@@ -56,9 +56,7 @@ defmodule Bolt.Paginator do
     # Add the navigator reactions to the embed.
     # Sleep shortly in between to respect ratelimits.
     {:ok} = Api.create_reaction(original_msg.channel_id, msg.id, "⬅")
-    Process.sleep(250)
     {:ok} = Api.create_reaction(original_msg.channel_id, msg.id, "➡")
-    Process.sleep(250)
     {:ok} = Api.create_reaction(original_msg.channel_id, msg.id, "❌")
 
     paginator_map = %{
