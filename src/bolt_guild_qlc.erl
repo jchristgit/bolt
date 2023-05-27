49ae4d9f850ea1f02eb45121bb96b88fa8b2a5bf
Johannes Christ <jc@jchri.st>
2023-05-27 11:54:39 +0200

    Correct sudo queries

--- src/bolt_guild_qlc.erl
+++ src/bolt_guild_qlc.erl
@@ -7,11 +7,11 @@
 count() ->
     Q = qlc:q([1 || _ <- ?CACHE:query_handle()]),
     ?CACHE:wrap_qlc(fun () ->
-        qlc:fold(0, fun (_, Acc) -> Acc + 1 end, Q)
+        qlc:fold(fun (_, Acc) -> Acc + 1 end, 0, Q)
     end).
 
 total_member_count() ->
     Q = qlc:q([Members || {_, #{member_count := Members}} <- ?CACHE:query_handle()]),
     ?CACHE:wrap_qlc(fun () ->
-        qlc:fold(0, fun (Count, Acc) -> Acc + Count end, Q)
+        qlc:fold(fun (Count, Acc) -> Acc + Count end, 0, Q)
     end).
