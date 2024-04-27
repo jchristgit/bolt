5ba4fd9bef8b4e8a6f54b8b50cf9de2d035ed640
Johannes Christ <jc@jchri.st>
2024-04-27 18:28:57 +0200

    Disable runtime config in tests
    This breaks the tests for whatever reason with just
    an exit being shown on the command line

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,34 +1,36 @@
 import Config
 
-config :bolt,
-  botlog_channel: System.get_env("BOTLOG_CHANNEL"),
-  ecto_repos: [Bolt.Repo],
-  prefix: System.get_env("BOT_PREFIX") || ".",
-  # rrd_directory: is set below
-  superusers:
-    (System.get_env("SUPERUSERS") || "")
-    |> String.split(":", trim: true)
-    |> Enum.map(fn user_id ->
-      {value, _} = Integer.parse(user_id)
-      value
-    end),
-  web_domain: System.get_env("WEB_DOMAIN")
+if config_env() != :test do
+  config :bolt,
+    botlog_channel: System.get_env("BOTLOG_CHANNEL"),
+    ecto_repos: [Bolt.Repo],
+    prefix: System.get_env("BOT_PREFIX") || ".",
+    # rrd_directory: is set below
+    superusers:
+      (System.get_env("SUPERUSERS") || "")
+      |> String.split(":", trim: true)
+      |> Enum.map(fn user_id ->
+        {value, _} = Integer.parse(user_id)
+        value
+      end),
+    web_domain: System.get_env("WEB_DOMAIN")
 
-# STATE_DIRECTORY is exported by systemd when we set the `StateDirectory` option.
-case System.get_env("STATE_DIRECTORY") do
-  nil ->
-    nil
+  # STATE_DIRECTORY is exported by systemd when we set the `StateDirectory` option.
+  case System.get_env("STATE_DIRECTORY") do
+    nil ->
+      nil
 
-  directory ->
-    config :bolt, :rrd_directory, List.last(String.split(directory, ":"))
+    directory ->
+      config :bolt, :rrd_directory, List.last(String.split(directory, ":"))
+  end
+
+  config :nosedrum,
+    prefix: System.get_env("BOT_PREFIX") || "."
+
+  config :bolt, Bolt.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    url: System.get_env("PGSQL_URL")
+
+  config :nostrum,
+    token: System.get_env("BOT_TOKEN")
 end
-
-config :nosedrum,
-  prefix: System.get_env("BOT_PREFIX") || "."
-
-config :bolt, Bolt.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("PGSQL_URL")
-
-config :nostrum,
-  token: System.get_env("BOT_TOKEN")
